6f3681fd0b21a8177adff852db6901b57ae9d1db
cacabo <ccabo@seas.upenn.edu>
2020-04-19 00:23:51 -0400

    add service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,18 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
 export const onClientEntry = () => {
-  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+  // IntersectionObserver polyfill for gatsby-background-image (for Safari, IE)
   if (!('IntersectionObserver' in window)) {
     import('intersection-observer')
     console.log('IntersectionObserver is polyfilled!')
+  }
+}
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This app has been updated. Reload to display the latest version?`,
+  )
+
+  if (answer) {
+    // Load the new version of the site
+    window.location.reload()
   }
 }
--- gatsby-config.js
+++ gatsby-config.js
@@ -19,15 +19,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `cameroncabo.com`,
+        short_name: `cameroncabo.com`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FFFFFF`,
+        theme_color: `#17718f`,
         display: `minimal-ui`,
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-offline',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
