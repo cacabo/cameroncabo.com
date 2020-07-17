c783aa52f775664d287274cb3e6002b26462eb2e
cacabo <cameroncabo@gmail.com>
2020-07-16 22:15:28 -0700

    remove service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,14 +5,3 @@ export const onClientEntry = () => {
     console.log('IntersectionObserver is polyfilled!')
   }
 }
-
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This app has been updated. Reload to display the latest version?`,
-  )
-
-  if (answer) {
-    // Load the new version of the site
-    window.location.reload()
-  }
-}
--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-offline',
+    `gatsby-plugin-remove-serviceworker`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
